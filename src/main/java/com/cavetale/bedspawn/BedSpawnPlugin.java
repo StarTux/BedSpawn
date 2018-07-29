77fa3c1ae7f5a4565db33b728385d53f56d1a167
StarTux <ths.loy@googlemail.com>
2018-07-29 07:17:49 +0200

    Source

---
+++ src/main/java/com/cavetale/bedspawn/BedSpawnPlugin.java
@@ -0,0 +1,23 @@
+package com.cavetale.bedspawn;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BedSpawnPlugin extends JavaPlugin {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
+        if (!(sender instanceof Player)) return true;
+        if (args.length != 0) return false;
+        Player player = (Player)sender;
+        if (player.getBedSpawnLocation() == null) {
+            player.sendMessage(ChatColor.RED + "You do not have a bed spawn. Sleep in a bed first.");
+            return true;
+        }
+        player.teleport(player.getBedSpawnLocation().add(0.5, 0.0, 0.5));
+        player.sendMessage(ChatColor.BLUE + "Welcome to your bed.");
+        return true;
+    }
+}
